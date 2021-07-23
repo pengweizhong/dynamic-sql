3735e3a620919d1a39209d9e1c7967be47036808
pengweizhong <pengwz@hotmail.com>
2021-07-23 10:09:33 +0800

    回复误提文件

--- src/main/java/com/pengwz/dynamic/sql/base/impl/SqlImpl.java
+++ src/main/java/com/pengwz/dynamic/sql/base/impl/SqlImpl.java
@@ -466,11 +466,10 @@ public class SqlImpl<T> implements Sqls<T> {
     }
 
     private void printSql(PreparedStatement preparedStatement) {
-//        if (log.isDebugEnabled()) {
-        String sqlToString = preparedStatement.toString();
-        System.out.println(sqlToString.substring(sqlToString.indexOf(':') + 1));
-//        log.debug(sqlToString.substring(sqlToString.indexOf(':') + 1));
-//        }
+        if (log.isDebugEnabled()) {
+            String sqlToString = preparedStatement.toString();
+            log.debug(sqlToString.substring(sqlToString.indexOf(':') + 1));
+        }
     }
 
     private void buildPageInfo(PageInfo<T> pageInfo, List<T> list, Integer totalSize) {
